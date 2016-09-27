6e6739ffa7f787a71082dac17a72cbdbf5b409ee
kickscar <kickscar@gmail.com>
2016-09-27 17:41:34 +0900

    완료!

--- src/com/bit2016/network/echo/EchoServer.java
+++ src/com/bit2016/network/echo/EchoServer.java
@@ -1,8 +1,61 @@
 package com.bit2016.network.echo;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketException;
+
 public class EchoServer {
-
+	private static final int PORT = 9000;
+	
 	public static void main(String[] args) {
+		ServerSocket serverSocket = null;
+		
+		try {
+			//1. Server Socket 생성
+			String localHostAddress = InetAddress.getLocalHost().getHostAddress();
+			serverSocket = new ServerSocket();
+			
+			//2. Binding
+			serverSocket.bind( new InetSocketAddress( localHostAddress, PORT ) );
+			log( "binding " + localHostAddress + ":" + PORT );
+			
+			//3. Accept
+			Socket socket = serverSocket.accept();
+			
+			try{
+				//4. IOStream 생성(받아오기)
+				BufferedReader br = new BufferedReader( new InputStreamReader( socket.getInputStream(), "UTF-8" ) );
+				PrintWriter pw = new PrintWriter( new OutputStreamWriter( socket.getOutputStream(), "UTF-8" ), true );
+				while( true ) {
+					String data = br.readLine();
+					if( data == null ) {
+						log( "closed by client" );
+						break;
+					}
+					
+					log( "received:" + data );
+					pw.println( data );
+				}
+			} catch( SocketException ex ){
+				log( "abnormal closed by client" );
+			} catch( IOException ex ) {
+				log( "error:" + ex );
+			} finally {
+				socket.close();
+			}
+		} catch( IOException ex ) {
+			log( "error:" + ex );
+		}
 	}
-
+	
+	public static void log( String message ) {
+		System.out.println( "[Echo Server]" + message );
+	}
 }
--- src/com/bit2016/network/util/NSLookup.java
+++ src/com/bit2016/network/util/NSLookup.java
@@ -2,21 +2,34 @@ package com.bit2016.network.util;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.util.Scanner;
 
 public class NSLookup {
 
 	public static void main(String[] args) {
+		Scanner scanner = null;
 		try {
-			String host = "www.naver.com";
-			InetAddress[] inetAddresses = InetAddress.getAllByName( host );
-			
-			for( InetAddress inetAddress : inetAddresses ){
-				System.out.println( inetAddress.getHostAddress() );
+			scanner = new Scanner(System.in);
+
+			while( true ) {
+				System.out.print(">>");
+				String line = scanner.nextLine();
+
+				if ("exit".equals(line)) {
+					break;
+				}
+
+				InetAddress[] inetAddresses = InetAddress.getAllByName(line);
+				for (InetAddress inetAddress : inetAddresses) {
+					System.out.println(line + ":" + inetAddress.getHostAddress());
+				}
 			}
 		} catch (UnknownHostException e) {
 			e.printStackTrace();
+		} finally {
+			if (scanner != null) {
+				scanner.close();
+			}
 		}
-
 	}
-
 }
