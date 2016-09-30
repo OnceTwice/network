31e45836c492ea5711cf647da2144c712e47cf62
kickscar <kickscar@gmail.com>
2016-09-30 14:06:20 +0900

    UDP Echo C/S

--- src/com/bit2016/network/udp/UDPTimeClient.java
+++ src/com/bit2016/network/udp/UDPTimeClient.java
@@ -1,9 +1,37 @@
 package com.bit2016.network.udp;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetSocketAddress;
+
 public class UDPTimeClient {
+	private static final String SERVER_IP = "192.168.1.2";
+	private static final int SERVER_PORT = 9000;
+	private static final int BUFFER_SIZE = 1024;
 
 	public static void main(String[] args) {
+		DatagramSocket socket = null;
 
+		try {
+			// 1. 소켓 생성
+			socket = new DatagramSocket();
+
+			// 2. 요청 패킷 전송
+			DatagramPacket sendPacket = new DatagramPacket("".getBytes(), 0, new InetSocketAddress( SERVER_IP, SERVER_PORT ));
+			socket.send( sendPacket );
+			
+			// 3. 시간 데이터 수신
+			DatagramPacket receivePacket = new DatagramPacket( new byte[ BUFFER_SIZE ], BUFFER_SIZE );
+			socket.receive( receivePacket );
+			
+			// 4. 출력
+			String now = new String( receivePacket.getData(), 0, receivePacket.getLength(), "UTF-8" );
+			System.out.println( now );
+			
+		} catch ( IOException e ) {
+			e.printStackTrace();
+		}
 	}
 
 }
--- src/com/bit2016/network/udp/UDPTimeServer.java
+++ src/com/bit2016/network/udp/UDPTimeServer.java
@@ -1,10 +1,39 @@
 package com.bit2016.network.udp;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.util.Date;
+import java.text.SimpleDateFormat;
+
 public class UDPTimeServer {
-	
-	
+	private static final int PORT = 9000;
+	private static final int BUFFER_SIZE = 0;
+
 	public static void main(String[] args) {
+		DatagramSocket socket = null;
 
+		try {
+			// 1. socket 생성
+			socket = new DatagramSocket(PORT);
+
+			while (true) {
+				// 2. 요청 패킷 수신
+				DatagramPacket receivePacket = new DatagramPacket(new byte[BUFFER_SIZE], BUFFER_SIZE);
+				socket.receive(receivePacket);
+
+				// 3. 시간 데이터 응답
+				String now = new SimpleDateFormat("yyyy-MM-dd a hh:mm:ss.SSS").format( new Date() );
+				byte[] sendData = now.getBytes();
+				DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, receivePacket.getAddress(), receivePacket.getPort());
+				socket.send(sendPacket);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (socket != null && socket.isClosed() == false) {
+				socket.close();
+			}
+		}
 	}
-
 }
